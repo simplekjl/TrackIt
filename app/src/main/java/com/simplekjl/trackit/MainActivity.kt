49c829bef2ef4b7bebfdba5a4a325fe9c752d784
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-02 13:18:46 +0200

    Added toolbar in compose

--- app/src/main/java/com/simplekjl/trackit/MainActivity.kt
+++ app/src/main/java/com/simplekjl/trackit/MainActivity.kt
@@ -3,13 +3,26 @@ package com.simplekjl.trackit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.simplekjl.trackit.ui.theme.TrackItTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +30,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TrackItTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                OverflowMenuTest()
             }
         }
     }
+
+}
+
+@Preview
+@Composable
+fun PreviewOverflowMenu() {
+    OverflowMenuTest()
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun OverflowMenuTest() {
+    var showMenu by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TrackItTheme {
-        Greeting("Android")
-    }
+    TopAppBar(
+        title = { Text("Track it") },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Default.Favorite, contentDescription = "Menu favorites")
+            }
+            IconButton(onClick = { showMenu = !showMenu }) {
+                Icon(Icons.Default.MoreVert, contentDescription = "MEnu more")
+            }
+            DropdownMenu(
+                expanded = showMenu,
+                onDismissRequest = { showMenu = false }
+            ) {
+                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Refresh, contentDescription = "refresh")
+                }
+                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Call, contentDescription = "call")
+                }
+            }
+        },
+        backgroundColor = Color.Transparent,
+        contentColor = Color.Gray,
+        elevation = 2.dp
+    )
 }

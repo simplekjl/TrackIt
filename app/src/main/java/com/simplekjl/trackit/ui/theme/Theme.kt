973fdd7577b60119946a7c46ef4396f389959e28
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-03 11:47:32 +0200

    theme basics

--- app/src/main/java/com/simplekjl/trackit/ui/theme/Theme.kt
+++ app/src/main/java/com/simplekjl/trackit/ui/theme/Theme.kt
@@ -7,24 +7,21 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.mid_blue,
+    primaryVariant = Color.blue,
+    secondary = Color.gray_4,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Color.mid_blue,
+    primaryVariant = Color.blue,
+    secondary = Color.gray_4,
+    background = Color.white,
+    surface = Color.white,
+    onPrimary = Color.white,
+    onSecondary = Color.black,
+    onBackground = Color.black,
+    onSurface = Color.black,
 )
 
 @Composable

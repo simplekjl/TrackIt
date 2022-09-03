eb78ac45230d7dbc9922b929eae6280eaa1b4a54
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-03 14:26:50 +0200

    Added small component

--- app/src/main/java/com/simplekjl/trackit/MainActivity.kt
+++ app/src/main/java/com/simplekjl/trackit/MainActivity.kt
@@ -3,6 +3,9 @@ package com.simplekjl.trackit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -10,12 +13,15 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.simplekjl.trackit.R.string
 import com.simplekjl.ui.theme.TrackItColors
 import com.simplekjl.ui.theme.TrackItTheme
+import com.simplekjl.ui.theme.TrackItTypography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,7 +49,7 @@ fun MainMenuOverflow() {
                 Icon(
                     Icons.Default.Settings,
                     tint = TrackItColors.white,
-                    contentDescription = stringResource(string.menu_settings_action_description)
+                    contentDescription = stringResource(R.string.menu_settings_action_description)
                 )
             }
         },
@@ -51,4 +57,37 @@ fun MainMenuOverflow() {
         contentColor = TrackItColors.gray_4,
         elevation = 0.dp
     )
+}
+
+@Preview
+@Composable
+fun WeightValueElementTest() {
+    WeightValueElement(
+        metricNameRes = R.string.weight_current_label,
+        weightValue = 74.0,
+        colorRes = TrackItColors.plum
+    )
+}
+
+@Composable
+fun WeightValueElement(
+    @StringRes metricNameRes: Int,
+    weightValue: Double,
+    colorRes: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = stringResource(id = metricNameRes),
+            style = TrackItTypography().h3,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+        // weight
+        Text(
+            text = weightValue.toString(),
+            style = TrackItTypography().h4,
+            color = colorRes,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
 }

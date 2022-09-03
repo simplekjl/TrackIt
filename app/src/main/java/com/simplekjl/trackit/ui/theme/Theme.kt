6e2d6b36c0e325983e6764146024553e7cd9804e
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-03 12:37:04 +0200

    updated theme

--- app/src/main/java/com/simplekjl/trackit/ui/theme/Theme.kt
+++ app/src/main/java/com/simplekjl/trackit/ui/theme/Theme.kt
@@ -1,41 +1,94 @@
 package com.simplekjl.trackit.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Colors
+import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
 
-private val DarkColorPalette = darkColors(
-    primary = Color.mid_blue,
-    primaryVariant = Color.blue,
-    secondary = Color.gray_4,
-)
+@Composable
+fun TrackItTheme(
+    colors: Colors = if (isSystemInDarkTheme()) darkColors() else lightColors(),
+    typography: TrackItTypography = TrackItTypography(),
+    shapes: Shapes = Shapes(),
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        LocalColors provides colors,
+        LocalTypography provides typography,
+        LocalShapes provides shapes
+    ) {
+        content()
+    }
+}
 
-private val LightColorPalette = lightColors(
-    primary = Color.mid_blue,
-    primaryVariant = Color.blue,
-    secondary = Color.gray_4,
-    background = Color.white,
+object MaterialTheme {
+    val colors: Colors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColors.current
+
+    val typography: TrackItTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTypography.current
+
+    val shapes: Shapes
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalShapes.current
+}
+
+internal val LocalColors = staticCompositionLocalOf { lightColors() }
+
+internal val LocalTypography = staticCompositionLocalOf { TrackItTypography() }
+
+internal val LocalShapes = staticCompositionLocalOf { Shapes() }
+
+//theming
+fun lightColors(
+    primary: androidx.compose.ui.graphics.Color = Color.mid_blue,
+    primaryVariant: androidx.compose.ui.graphics.Color = Color.blue,
+    background: androidx.compose.ui.graphics.Color = androidx.compose.ui.graphics.Color.White,
+    secondary: androidx.compose.ui.graphics.Color = Color.gray_4
+): Colors = Colors(
+    primary = primary,
+    primaryVariant = primaryVariant,
+    secondary = secondary,
+    secondaryVariant = secondary,
+    background = background,
     surface = Color.white,
+    error = Color.plum,
     onPrimary = Color.white,
     onSecondary = Color.black,
     onBackground = Color.black,
+    onError = Color.black,
     onSurface = Color.black,
+    isLight = true
 )
 
-@Composable
-fun TrackItTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
-}
+// TODO update the black theme colors
+fun darkColors(
+    primary: androidx.compose.ui.graphics.Color = Color.mid_blue,
+    primaryVariant: androidx.compose.ui.graphics.Color = Color.blue,
+    background: androidx.compose.ui.graphics.Color = androidx.compose.ui.graphics.Color.White,
+    secondary: androidx.compose.ui.graphics.Color = Color.gray_4
+): Colors = Colors(
+    primary = primary,
+    primaryVariant = primaryVariant,
+    secondary = secondary,
+    secondaryVariant = secondary,
+    background = background,
+    surface = Color.white,
+    error = Color.plum,
+    onPrimary = Color.white,
+    onSecondary = Color.white,
+    onBackground = Color.black,
+    onError = Color.black,
+    onSurface = Color.black,
+    isLight = false
+)
--- app/src/main/java/com/simplekjl/trackit/ui/theme/Type.kt
+++ app/src/main/java/com/simplekjl/trackit/ui/theme/Type.kt
@@ -1,28 +1,131 @@
 package com.simplekjl.trackit.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.simplekjl.trackit.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.roboto_black, FontWeight.Black),
+    Font(R.font.roboto_black_italic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.roboto_thin, FontWeight.Thin),
+    Font(R.font.roboto_thin_italic, FontWeight.Thin, FontStyle.Italic)
+)
+
+
+@Immutable
+data class TrackItTypography(
+    val h1: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 72.sp,
+        lineHeight = 72.sp,
+        letterSpacing = (-0.0208).em
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    val h2: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 60.sp,
+        lineHeight = 60.sp,
+        letterSpacing = (-0.0083).em
+    ),
+    val h3: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 48.sp,
+        lineHeight = 48.sp
+    ),
+    val h4: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 36.sp,
+        lineHeight = 38.sp,
+        letterSpacing = (0.007).em
+    ),
+    val h5: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 28.sp
+    ),
+    val h6: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 21.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 24.sp,
+        letterSpacing = (0.007).em
+    ),
+    val headline: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 22.sp
+    ),
+    val bodyStrong: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 20.sp,
+        letterSpacing = (0.0155).em
+    ),
+    val body: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 16.sp,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        lineHeight = 20.sp,
+        letterSpacing = (0.0155).em
+    ),
+    val subhead: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 20.sp,
+        letterSpacing = (0.007).em
+    ),
+    val text: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 20.sp,
+        letterSpacing = (0.007).em
+    ),
+    val footnote: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 13.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 18.sp,
+        letterSpacing = (-0.006).em
+    ),
+    val caption: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 16.sp,
+        letterSpacing = (0.033).em
+    ),
+    val button: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 16.sp,
+        letterSpacing = (0.089).em
+    ),
+    val overline: TextStyle = TextStyle(
+        fontFamily = Roboto,
+        fontSize = 10.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 12.sp,
+        letterSpacing = (0.15).em
     )
-    */
 )

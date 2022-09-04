c04f3ddc65be800176dc7e792743ea5e74b8e2b0
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-05 01:17:34 +0200

    Added chart item WIP

--- common/ui/src/main/java/com/simplekjl/ui/theme/components/Atoms.kt
+++ common/ui/src/main/java/com/simplekjl/ui/theme/components/Atoms.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
@@ -23,9 +24,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.simplekjl.ui.R
 import com.simplekjl.ui.theme.base.TrackItColors
 import com.simplekjl.ui.theme.base.TrackItTheme
@@ -77,10 +85,10 @@ fun WeightValueElementTest() {
 
 @Composable
 fun WeightValueElement(
+    modifier: Modifier = Modifier,
     @StringRes metricNameRes: Int,
     weightValue: Double,
-    colorRes: Color,
-    modifier: Modifier = Modifier
+    colorRes: Color
 ) {
     Column(
         modifier = modifier
@@ -122,9 +130,9 @@ fun ColorDescriptionPreview() {
 
 @Composable
 fun ColorDescription(
+    modifier: Modifier = Modifier,
     @StringRes descriptionRes: Int,
     color: Color,
-    modifier: Modifier = Modifier,
     shape: Shape = RoundedCornerShape(1.dp)
 ) {
     Row(
@@ -144,4 +152,56 @@ fun ColorDescription(
             modifier = Modifier.padding(start = 8.dp), style = TrackItTypography().caption
         )
     }
+}
+
+@Preview
+@Composable
+fun LinearChartProgressTest() {
+    val entries: MutableList<Entry> = mutableListOf()
+    val c1e1 = Entry(0f, 100000f) // 0 == quarter 1
+    entries.add(c1e1)
+    val c1e2 = Entry(1f, 140000f) // 1 == quarter 2 ...
+    entries.add(c1e2)
+
+    // ...
+    TrackItTheme {
+        LinearChartProgress(
+            weightValues = entries,
+            topLimitLabel = R.string.app_name,
+            bottomLimitLabel = R.string.weight_target_label,
+            lineColor = TrackItColors.mint
+        )
+    }
+}
+
+@Composable
+fun LinearChartProgress(
+    modifier: Modifier = Modifier,
+    weightValues: List<Entry> = emptyList(),
+    @StringRes topLimitLabel: Int,
+    @StringRes bottomLimitLabel: Int,
+    lineColor: Color,
+) {
+    val topLabel = stringResource(id = topLimitLabel)
+    val bottomLabel = stringResource(id = bottomLimitLabel)
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = { context -> LineChart(context) },
+        update = { linearChart ->
+            val dataSet = LineDataSet(weightValues, topLabel).apply {
+                color = lineColor.toArgb()
+            }
+            val linearData = LineData(dataSet)
+            linearChart.data = linearData
+            // grid properties
+            // https://stackoverflow.com/questions/31263097/mpandroidchart-hide-background-grid
+//            linearChart.xAxis.isEnabled = false
+            linearChart.axisRight.setDrawGridLines(false)
+            linearChart.axisRight.isEnabled = false
+            linearChart.xAxis.setDrawGridLines(false)
+            linearChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+            linearChart.invalidate()
+        }
+    )
 }

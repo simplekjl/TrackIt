aad727388f37ac45e9d30de71ee41657b5967309
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-20 22:41:07 +0200

    Created Measures Updated Weight class

---
+++ domain/src/main/java/com/simplekjl/domain/model/Entities.kt
@@ -0,0 +1,29 @@
+package com.simplekjl.domain.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "weight", primaryKeys = ["uid", "date"])
+data class Weight(
+    @PrimaryKey(autoGenerate = true) val uid: Int,
+    @ColumnInfo(name = "date") val date: Long,
+    @ColumnInfo(name = "weight") val weight: Double,
+    @ColumnInfo(name = "note") val note: String?
+)
+
+// User should be able to also create measurements without tracking weight
+@Entity(primaryKeys = ["entryUid", "date"])
+data class Measures(
+    @PrimaryKey val entryUid: Int,
+    val date: Long,
+    val chest: Double,
+    val leftArm: Double,
+    val rightArm: Double,
+    val waist: Double,
+    val hips: Double,
+    val leftThigh: Double,
+    val rightThigh: Double,
+    val leftCalf: Double,
+    val rightCalf: Double
+)

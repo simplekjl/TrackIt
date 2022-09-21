8d81982ea4e167f2b351fc1ee6a52533bd6f4eb6
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-21 11:22:17 +0200

    updated tests

--- app/src/androidTest/kotlin/com/simplekjl/trackit/framework/WeightDatabaseTest.kt
+++ app/src/androidTest/kotlin/com/simplekjl/trackit/framework/WeightDatabaseTest.kt
@@ -6,7 +6,11 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.appmattus.kotlinfixture.decorator.nullability.NeverNullStrategy
+import com.appmattus.kotlinfixture.decorator.nullability.nullabilityStrategy
+import com.appmattus.kotlinfixture.kotlinFixture
 import com.google.common.truth.Truth.assertThat
+import com.simplekjl.domain.model.Measures
 import com.simplekjl.domain.model.Weight
 import org.junit.After
 import org.junit.Before
@@ -22,8 +26,12 @@ class WeightDatabaseTest {
     @get:Rule
     var instantTaskExecutor = InstantTaskExecutorRule()
 
-    private lateinit var userDao: WeightDao
+    private lateinit var weightDao: WeightDao
     private lateinit var db: WeightDatabase
+
+    private val fixture = kotlinFixture {
+        nullabilityStrategy(NeverNullStrategy)
+    }
 
     @Before
     fun createDb() {
@@ -31,17 +39,28 @@ class WeightDatabaseTest {
         db = Room.inMemoryDatabaseBuilder(
             context, WeightDatabase::class.java
         ).build()
-        userDao = db.getWeightDao()
+        weightDao = db.getWeightDao()
     }
 
     @Test
-    fun writeUserAndReadInList() {
+    fun writeWeightAndReadInList() {
         createDb()
         val time = System.currentTimeMillis()
-        val user: Weight = Weight(12, time, 45.0, "")
-        userDao.insertAll(user)
-        val byName = userDao.findByDate(time)
-        assertThat(byName.weight == user.weight).isTrue()
+        val weight = Weight(12, time, 45.0, "")
+        weightDao.insertAllWeights(weight)
+        val byName = weightDao.findWeightByDate(time)
+        assertThat(byName.weight == weight.weight).isTrue()
+    }
+
+    @Test
+    fun writeMeasureAndReadInList() {
+        createDb()
+        val time = System.currentTimeMillis()
+        var measure: Measures = fixture()
+        measure = measure.copy(date = time)
+        weightDao.insertAllMeasures(measure)
+        val byName = weightDao.getMeasureByDate(time)
+        assertThat(byName.leftArm == measure.leftArm).isTrue()
     }
 
     @After

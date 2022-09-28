8834af47422e25c7331e391e46968c81aab78b0e
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-28 11:07:42 +0200

    Added column info to measures

--- data/src/main/java/com/simplekjl/data/model/Entities.kt
+++ data/src/main/java/com/simplekjl/data/model/Entities.kt
@@ -25,17 +25,28 @@ data class ProfileRaw(
 )
 
 // User should be able to also create measurements without tracking weight
-@Entity(tableName = "measures", primaryKeys = ["entryUid", "date"])
+@Entity(tableName = "measures")
 data class MeasuresRaw(
+    @PrimaryKey(autoGenerate = true)
     val entryUid: Int,
+    @ColumnInfo(name = "date")
     val date: Long,
+    @ColumnInfo(name = "chest")
     val chest: Double,
+    @ColumnInfo(name = "leftArm")
     val leftArm: Double,
+    @ColumnInfo(name = "rightArm")
     val rightArm: Double,
+    @ColumnInfo(name = "waist")
     val waist: Double,
+    @ColumnInfo(name = "hips")
     val hips: Double,
+    @ColumnInfo(name = "leftThigh")
     val leftThigh: Double,
+    @ColumnInfo(name = "rightThigh")
     val rightThigh: Double,
+    @ColumnInfo(name = "leftCalf")
     val leftCalf: Double,
+    @ColumnInfo(name = "rightCalf")
     val rightCalf: Double
 )
